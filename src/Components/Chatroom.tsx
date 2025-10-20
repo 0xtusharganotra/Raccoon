5d99bb086b9ba3eaab1ebb855a9406d56f9d24de
0xtusharganotra <ganotra.vox@gmail.com>
2025-10-20 20:51:08 +0530

    UI: Create Chat UI

--- src/Components/Chatroom.tsx
+++ src/Components/Chatroom.tsx
@@ -1,14 +1,19 @@
+import Send from "@/icons/Send";
+
 const Chatroom = () => {
   return (
-    <div className="w-[82%] h-[100%] bg-custom-bg">
-      <div className="w-[100%] h-[100%] flex justify-center items-center">
-        <div className="w-[50%] h-auto px-4 py-4 bg-black rounded-lg shadow-xl shadow-green-700 text-white flex flex-col items-center item-center mx-auto my-auto justify-center gap-1">
-          <img src="assets\Raccoon-logo.png" className="w-[200px]" alt="" />
-          <p className="text-2xl">Welcome to Raccoon Chat Rooms</p>
-          <p className="text-gray-400 pb-2 text-center">
-            Join any chat room to connect with others - learn, share, and
-            exchange your experiences.
-          </p>
+    <div className="w-[82%] h-[100%] bg-custom-bg flex flex-col">
+      <div className="w-full h-[calc(100%-40px)]"></div>
+      <div className="mt-0 mb-4 h-[40px] flex justify-around">
+        <div className="w-[90%] h-[40px] bg-[#111111] rounded-xl px-4 flex justify-between items-center">
+          <input
+            type="text"
+            className="w-[70%] h-[30px] text-gray-300 outline-none "
+            placeholder="Enter text here..."
+          />
+          <button className="cursor-pointer">
+            <Send />
+          </button>
         </div>
       </div>
     </div>
